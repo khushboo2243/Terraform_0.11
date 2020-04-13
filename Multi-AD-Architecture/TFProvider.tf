60d5c47b284a8a2e7a4574823d461fa6c6154f32
Khushboo Nigam <khushboo2243@gmail.com>
2020-04-13 15:38:14 -0400

    adding bastion pvt key variable

--- Multi-AD-Architecture/TFProvider.tf
+++ Multi-AD-Architecture/TFProvider.tf
@@ -5,6 +5,7 @@ provider "oci" {
    fingerprint = "${var.fingerprint}"
    private_key_path = "${var.private_key_path}"
    region = "${var.region}"
+   bastion_ssh_private_key= "${var.bastion_ssh_private_key}"
    disable_auto_retries = "false"
 
    //version = "=3.30.0"
